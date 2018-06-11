edb1e181dbb8ef74542adb624c9294feff618205
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-06-11 22:45:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,4 +33,8 @@ WIN_COMBINATIONS = [
   def position_taken?(index)
     !(@board[index].nil? || @board[index] == " ")
   end
+
+  def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+  end
 end
