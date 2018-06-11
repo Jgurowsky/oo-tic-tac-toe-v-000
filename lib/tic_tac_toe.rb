15780e350535e3f7e6dd807bd740f8d61ec95486
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-06-11 22:30:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,6 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def WIN_COMBINATIONS
     WIN_COMBINATIONS = [
  [0,1,2],
  [3,4,5],
@@ -14,5 +13,6 @@ class TicTacToe
  [0,4,8],
  [2,4,6]
 ]
-  end
+
+
 end
